6fead03898acc4cf86a05bf2e8c36f05192aceec
Ovadia Esquenazi <ovadia.esq@gmail.com>
2021-11-28 23:52:32 +0200

    test

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyparser = require('body-parser')
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const value = require('./routes/route')
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors())
+app.use(bodyparser.json())
 
 // dev logging middleware
 if (process.env.NODE_ENV === 'development') {
