e37404c4598d04ff56ec07f999769ec775b29e22
Ovadia Esquenazi <ovadia.esq@gmail.com>
2021-11-28 20:02:58 +0200

    testing

--- controllers/goldvalue.js
+++ controllers/goldvalue.js
@@ -3,9 +3,9 @@ let gold = 0;
 exports.getGoldValue = async (req, res, next) => {
     res.status(200).json({
         success:true,
-        data: req.body
+        data: req.body.value
     })
-    gold = req.body.data.value
+    gold = req.body.value
 }
 
 exports.displayGoldValue = async (req, res, next) => {
