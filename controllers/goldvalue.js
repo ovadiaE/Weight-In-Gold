072f880e978cf2016b478846a1cd69d2d9c94e18
Ovadia Esquenazi <ovadia.esq@gmail.com>
2021-11-28 19:43:41 +0200

    Seeing gold?

--- controllers/goldvalue.js
+++ controllers/goldvalue.js
@@ -1,19 +1,19 @@
-let data = 0;
+let gold = 0;
 
 exports.getGoldValue = async (req, res, next) => {
     res.status(200).json({
         success:true,
         data: req.body
-    }).send(req.body)
-    console.log(req.body)
+    })
+    gold = req.body.value
 }
 
 exports.displayGoldValue = async (req, res, next) => {
-    console.log(res)
     const value = {
         "value": {
-            "data": data,
+            "data": gold,
        }
     }
+    console.log(value)
     res.status(200).send(value)
 }
