fe06beb4af2bb6340139f933e807e3e63b893f12
Ovadia Esquenazi <ovadia.esq@gmail.com>
2021-11-29 00:39:50 +0200

    checking

--- controllers/goldvalue.js
+++ controllers/goldvalue.js
@@ -1,8 +1,9 @@
 let body;
-
 exports.getGoldValue = async (req, res, next) => {
     try {
-         body =  req.body
+         body = req.body
+         console.log(req)
+         console.log(req.body)
          res.status(200).json(body)
 
     } catch(error){
@@ -13,6 +14,7 @@ exports.getGoldValue = async (req, res, next) => {
 }
 
 exports.displayGoldValue = async (req, res, next) => {
+    console.log(body)
     try {res.status(200).json(body)} 
     
     catch (error) {
